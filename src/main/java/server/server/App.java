e9dd9c930e94990dcc6a0b43e5f0eb03afe5a700
Jose Angel Morena <joseangel.morena@estudiants.urv.cat>
2019-03-27 22:11:24 +0100

    por defecto al arrancar se pone en el puerto 80;\nhttp://localhost/getData?id=2&second=something

--- src/main/java/server/server/App.java
+++ src/main/java/server/server/App.java
@@ -1,10 +1,22 @@
 package server.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class App {
    public static void main(String[] args) {
       SpringApplication.run(App.class, args);
+   }
+   
+   @Component
+   public class CustomContainer implements EmbeddedServletContainerCustomizer {
+
+       public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+          configurableEmbeddedServletContainer.setPort(80);
+       }
+
    }
 }
